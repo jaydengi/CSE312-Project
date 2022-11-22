17155f99db1a2628bc3bcb2403e56b63eebb661a
dcnorkai <dcnorkai@buffalo.edu>
2022-11-21 20:47:09 -0500

    added login and items listing pages stubs

--- routes/pages.js
+++ routes/pages.js
@@ -8,5 +8,19 @@ router.get('/', function(req, res) {
     });
 });
 
+// Login Page
+router.get('/login', function(req, res) {
+    res.render('login', {
+        title: 'Login'
+    });
+});
+
+// Item Listings Page
+router.get('/items', function(req, res) {
+    res.render('items', {
+        title: 'Item Listings'
+    });
+});
+
 // Exports
 module.exports = router;
