f365aa9024e17c7ddbf2d55a98d8d436204e5200
dcnorkai <dcnorkai@buffalo.edu>
2022-11-28 19:04:47 -0500

    almost finished item listings, images are broken atm

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// Init Express + Mongoose
 var express = require('express');
-var bodyParser = require("body-parser");
 var path = require('path');
-const mongoose = require('mongoose');
-var Schema = mongoose.Schema
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express();
 var fs = require('fs');
 require('dotenv/config');
 
@@ -14,102 +15,26 @@ db.once('open', function() {
     console.log('Connected to MongoDB')
 });
 
-// Init app
-const app = express();
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// EJS setup
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Set public folder
+console.log(path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set uploads folder
+console.log(path.join(__dirname, 'uploads'));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 // Set routes
 var pages = require('./routes/pages.js');
-
-var multer = require('multer');
-  
-// var storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         console.log(path.join(__dirname, 'uploads'))
-//         cb(null, path.join(__dirname, 'uploads'))
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-  
-var upload = multer({ 
-    storage: multer.diskStorage ({
-        destination: (req, file, cb)=>{
-            cb (null, './uploads')
-        }
-    }) 
-});
-
-var imgModel = require('model');
-
 app.use('/', pages);
-
-app.get('/item-added', (req, res) => {
-    imgModel.find({}, (err, items) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('An error occurred', err);
-        }
-        else {
-            res.render('items', { items: items });
-        }
-    });
-});
-
-app.post('/item-added', upload.single('image'), (req, res, next) => {
-    console.log('req has ', req.body)
-    var obj = {
-        name: req.body.itemname,
-        price: req.body.price,
-        description: req.body.description,
-        img: {
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.body.upload)),
-            contentType: 'image/png'
-        }
-    }
-    imgModel.create(obj, (err, item) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // item.save();
-            res.redirect('/items');
-        }
-    });
-});
-
-// app.post('/item-added', function(req,res) {
-//     var name = req.body.itemname;
-//     var price = req.body.price;
-//     var description = req.body.description;
-//     var imagefile = req.body.imagefile;
-  
-//     var item = {
-//         "name": name,
-//         "price":price,
-//         "description":description,
-//         "image":imagefile
-//     }
-//     db.collection('Items').insertOne(item,function(err, collection){
-//         if (err) throw err;
-//         console.log(item);
-//         console.log("Item inserted Successfully");     
-//     });
-//     return res.redirect('/items');
-// })
+app.use('/add-item', pages);
 
 // Start the server
 var port = 3000;
--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser')
-//var jsonParser = bodyParser.json()
+var path = require('path');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var mongoose = require('mongoose');
+var multer = require('multer');
+require('dotenv/config');
 
+// Item schema
+var itemSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String,
+    img: String
+    // img:
+    // {
+    //     data: Buffer,
+    //     contentType: String
+    // }
+});
+itemModel = mongoose.model('item', itemSchema);
+
+// Multer setup
+var upload = multer ({
+    storage : multer.diskStorage ({
+        destination: (req, file, cb)=>{
+            cb (null, 'uploads/')
+        },
+        filename : function (req, file, callback) {
+            callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        }
+    })
+})
 
 // Homepage
 router.get('/', function(req, res) {
@@ -21,9 +49,13 @@ router.get('/login', function(req, res) {
 
 // Item Listings Page
 router.get('/items', function(req, res) {
-    res.render('items', {
-        title: 'Item Listings'
-    });
+    itemModel.find()
+        .then(function(doc){
+            res.render('items', {
+                title: 'Item Listings',
+                item : doc
+            })
+        })
 });
 
 // Shopping Cart Page
@@ -38,6 +70,23 @@ router.get('/add-item', function(req, res) {
     res.render('additem', {
         title: 'Add Item'
     });
+});
+router.post('/post', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    var temp = new itemModel();
+    temp.name = req.body.name;
+    temp.price = req.body.price;
+    temp.description = req.body.description;
+    temp.img = req.file.filename;
+    temp.save((err, doc)=>{
+        if (!err) {
+            console.log("Item saved successfully");
+            res.redirect('/items');
+        }
+        else {
+            console.log(err);
+        }
+    })
 });
 
 // Registration Page
